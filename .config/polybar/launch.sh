92d75c2138bde810a74acb0dbe7a104b21a14b4d
Anormix <80751674+Anormix@users.noreply.github.com>
2024-06-27 20:24:05 +0300

    added zathura

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,5 +10,5 @@ polybar-msg cmd quit
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar invis 2>&1 | tee -a /tmp/polybar1.log & disown
-
+polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
 echo "Bars launched..."
